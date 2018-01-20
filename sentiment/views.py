6f4627c660a723463394bf01b71639eab86de62a
Animesh Risal <animeshrisal99@gmail.com>
2018-01-20 08:20:52 +0545

    Added more templates

--- sentiment/views.py
+++ sentiment/views.py
@@ -4,15 +4,15 @@ from .models import tweets
 
 # Create your views here.
 def index(request):
+
+    return render(request, 'index.html')
+
+def chart(request):
     tweets.objects.all().delete()
     for x in getHashTagTweets('obama'):
         tweet = tweets()
         tweet.tweets = x.text
         tweet.date = x.created_at
         tweet.save()
-
-    return render(request, 'index.html')
-
-def chart(request):
     tweet = tweets.objects.all()
     return render(request, 'chart.html', {'tweet': tweet})
