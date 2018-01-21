1a639313318872d8e2ef6738dd2dabacad1dc940
Animesh Risal <animeshrisal99@gmail.com>
2018-01-21 12:31:51 +0545

    added template to chart.html

--- sentiment/views.py
+++ sentiment/views.py
@@ -1,18 +1,22 @@
 from django.shortcuts import render
 from sentiment.sentiment_analysis.tweetmining import getHashTagTweets
 from .models import tweets
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 def index(request):
-
     return render(request, 'index.html')
 
+@csrf_exempt
 def chart(request):
     tweets.objects.all().delete()
-    for x in getHashTagTweets('obama'):
+    #print(request.POST["hash"])
+    
+    for x in getHashTagTweets(request.POST["hash"]):
         tweet = tweets()
         tweet.tweets = x.text
         tweet.date = x.created_at
         tweet.save()
     tweet = tweets.objects.all()
-    return render(request, 'chart.html', {'tweet': tweet})
+
+    return render(request, 'chart.html', {'tweets': tweet})
