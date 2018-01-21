01f175efacfcf59be935061efb325cb75eabf162
Animesh Risal <animeshrisal99@gmail.com>
2018-01-21 14:41:59 +0545

    Created getNews function

--- sentiment/sentiment_analysis/newsmining.py
+++ sentiment/sentiment_analysis/newsmining.py
@@ -1,8 +1,9 @@
 import requests
 import json
 from keys import newsapi
-response = requests.get("https://newsapi.org/v2/everything?q=trump&from=2018-1-14&to=2018-1-21&language=en&apiKey=" + newsapi)
-response_data = response.json()
-for x in response_data['articles']:
-    print(x['description'], ' ', x['publishedAt'])
-    print('\n')
+
+
+def getNews():
+    response = requests.get("https://newsapi.org/v2/everything?q=trump&from=2018-1-14&to=2018-1-21&language=en&apiKey=" + newsapi)
+    response_data = response.json()
+    return response_data
